42b10b2a2aeeb7fe0320b65e84f81216d918b172
Tero Pikala <tero@pikala.com>
2025-05-20 14:18:59 +0100

    Add more tools (hopefully ChatGPT didn't invent too many of them).

--- src/utils/toolsData.ts
+++ src/utils/toolsData.ts
@@ -1,10 +1,11 @@
 import { MakitaTool } from '../types';
 import toolsJson from '../tools.json';
 
-// Transform the tools from JSON to match the MakitaTool interface
-export const makitaTools: MakitaTool[] = toolsJson.map((tool, index) => ({
-  name: tool.name,
-  model: tool.model,
-  description: tool.description,
-}));
-
+// Transform the tools from JSON to match the MakitaTool interface and sort by name
+export const makitaTools: MakitaTool[] = toolsJson
+  .map((tool, index) => ({
+    name: tool.name,
+    model: tool.model,
+    description: tool.description,
+  }))
+  .sort((a, b) => a.name.localeCompare(b.name));
